193877e77a5b7b8284774e78972af077fdcfacb1
Diêgo Ribeiro Melo <110476181+diegoribeiromelo@users.noreply.github.com>
2024-02-12 17:40:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+
+$(document).ready(function() {
+    $('#telefone').mask('(00) 00000-0000');
+    $('#cep').mask('00000-000');
+    $('#cpf').mask('000.000.000-00', {reverse: true});
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                    required: true,
+                    email: true
+                },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            }
+        },
+        messages: {
+            nome: "Por favor, insira o seu nome",
+            email: "Por favor, insira o seu e-mail",
+            telefone: "Por favor, insira o seu telefone",
+            cpf: "Por favor, insira o seu CPF",
+            endereco: "Por favor, insira o seu endereço",
+            cep: "Por favor, insira o seu CEP"  
+        },
+
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+})
